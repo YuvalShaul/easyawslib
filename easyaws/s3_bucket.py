d3d13e3769764afc4eca071a447feaeaa2481940
Yuval Shaul <yuval.shaul@gmail.com>
2021-03-07 11:35:20 +0200

    S3 code update

---
+++ easyaws/s3_bucket.py
@@ -0,0 +1,84 @@
+import json
+import boto3
+import util
+
+
+class S3Bucket:
+    def __init__(self, bucket_name, aws_access_key_id=None, aws_secret_access_key=None, region=None):
+        confdata = util.get_conf()
+        aws_access_key_id = aws_access_key_id or confdata['access_key_id']
+        aws_secret_access_key = aws_secret_access_key or confdata['secret_access_key']
+
+        self.s3_client = boto3.client('s3', aws_access_key_id=aws_access_key_id,
+                                      aws_secret_access_key=aws_secret_access_key,
+                                      region_name=region)
+        self.s3_resource = boto3.resource('s3', aws_access_key_id=aws_access_key_id,
+                                      aws_secret_access_key=aws_secret_access_key,
+                                          region_name=region)
+        self.bucket_name = bucket_name
+        self.region = region
+
+    def upload_file(self, *, file_path, key_name ):
+        self.s3_resource.meta.client.upload_file(Filename=file_path, Bucket=self.bucket_name, Key=key_name)
+
+    def get_bucket_file_url(self, fname_key):
+        bucket_location = self.s3_client.get_bucket_location(Bucket=self.bucket_name)['LocationConstraint']
+        object_url = f"https://{self.bucket_name}.s3.{bucket_location}.amazonaws.com/{fname_key}"
+        return object_url
+
+    def get_bucket_s3_uri(self):
+        return f's3://{self.bucket_name}/'
+
+    def get_S3_bucket(self):
+        bucket_names = [bucket['Name'] for bucket in self.get_other_buckets()[0]]
+        if self.bucket_name in bucket_names:
+            return
+
+        # print(f'Bucket {BUCKET_NAME} not found. Creating...')
+        self.s3_client.create_bucket(
+            ACL='private',
+            CreateBucketConfiguration={'LocationConstraint': self.region},
+            Bucket=self.bucket_name
+        )
+
+    def set_policy(self):
+        bucket_policy =  {
+            "Version": "2012-10-17",
+            "Id": "Full public",
+            "Statement": [
+                {
+                    "Sid": "Allow All",
+                    "Effect": "Allow",
+                    "Principal": "*",
+                    "Resource": "*",
+                    "Action": [
+                        "s3:*"
+                    ],
+                    "Resource": "arn:aws:s3:::bignet-eu-central-1/*"
+                }
+            ]
+        }
+
+        # Convert the policy from JSON dict to string
+        bucket_policy = json.dumps(bucket_policy)
+
+        # Set the new policy
+        self.s3_client.put_bucket_policy(Bucket=self.bucket_name, Policy=bucket_policy)
+
+
+    def get_other_buckets(self):
+        ans = self.s3_client.list_buckets()
+        bucket_list = ans['Buckets']
+        bucket_owner = ans['Owner']
+        return bucket_list, bucket_owner
+
+
+if __name__ == '__main__':
+    my_s3 = S3Bucket(bucket_name='my-first-bucket-84629694625', region='eu-central-1')
+    my_s3.get_S3_bucket()
+    # my_s3.set_policy()
+    buckets = my_s3.get_other_buckets()
+    url = my_s3.get_bucket_file_url('file1.txt')
+    print(my_s3.s3_client.meta.endpoint_url)
+
+
