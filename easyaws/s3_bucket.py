2c814dc14c22424e0e09d2e71ee8d26443d90201
Yuval Shaul <yuval.shaul@gmail.com>
2021-04-12 01:51:54 -0400

    adding file listing function

--- easyaws/s3_bucket.py
+++ easyaws/s3_bucket.py
@@ -27,19 +27,28 @@ class S3Bucket:
         object_url = f"https://{self.bucket_name}.s3.{bucket_location}.amazonaws.com/{fname_key}"
         return object_url
 
+    def list_all_files(self):
+        response = self.s3_client.list_objects(Bucket=self.bucket_name)
+        return response['Contents']
+
     def get_bucket_s3_uri(self):
         return f's3://{self.bucket_name}/'
 
     def get_S3_bucket(self):
-        other_buckets = self.get_other_buckets()
-        bucket_names = [bucket['Name'] for bucket in other_buckets[0]]
+        '''
+        Make sure a bucket (whose name is in self.bucket_name) was created.
+        '''
+        all = self.s3_resource.buckets.all()
+        all_buckets = [bucket for bucket in all]
+        # other_buckets = self.get_other_buckets()
+        bucket_names = [bucket.name for bucket in all_buckets]
         if self.bucket_name in bucket_names:
             return
 
         # print(f'Bucket {BUCKET_NAME} not found. Creating...')
         self.s3_client.create_bucket(
             ACL='private',
-            CreateBucketConfiguration={'LocationConstraint': self.region},
+            # CreateBucketConfiguration={'LocationConstraint': self.region},
             Bucket=self.bucket_name
         )
 
@@ -80,8 +89,10 @@ if __name__ == '__main__':
     my_s3 = S3Bucket(bucket_name='my-first-bucket-84629694625')
     my_s3.get_S3_bucket()
     my_s3.set_policy()
-    buckets = my_s3.get_other_buckets()
-    url = my_s3.get_bucket_file_url('file1.txt')
-    print(my_s3.s3_client.meta.endpoint_url)
+    # buckets = my_s3.get_other_buckets()
+    # url = my_s3.get_bucket_file_url('file1.txt')
+    # print(my_s3.s3_client.meta.endpoint_url)
+    all_files = my_s3.list_all_files()
+    print(all)
 
 
