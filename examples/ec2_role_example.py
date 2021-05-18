c0bca0c3122e20bdbd7488419dd7ac7e9d19c301
Yuval Shaul <yuval.shaul@gmail.com>
2021-05-18 10:41:06 -0400

    first version of ex2 role example

--- examples/ec2_role_example.py
+++ examples/ec2_role_example.py
@@ -6,20 +6,13 @@ Use:
 '''
 
 from easyaws.ec2_vm import Ec2Tool
-import easyaws.util
 
-def demo_ec2_role(my_ec2):
-    a, b = my_ec2.get_credentials()
-    print(a,b)
 
 
 
 def main_role_demo():
-    confdata = easyaws.util.get_conf('easyaws_conf.json')
-    my_ec2_tool = Ec2Tool(aws_access_key_id=confdata['access_key_id'],
-                          aws_secret_access_key = confdata['secret_access_key'],
-                          region = confdata['region'])
-    creds = my_ec2_tool.get_credentials()
+    creds = Ec2Tool.get_credentials()
+    print('credentials:', creds)
     return creds
 
 
