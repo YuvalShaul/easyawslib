9f5219e6143ca19aa202be198d2b3d774d7863f7
Yuval Shaul <yuval.shaul@gmail.com>
2021-05-20 10:53:53 -0400

    cleaning example

--- examples/ec2_role_example/ec2_role_example.py
+++ examples/ec2_role_example/ec2_role_example.py
@@ -1,6 +1,5 @@
 from easyaws.ec2_vm import Ec2Tool
 from easyaws.s3_bucket import S3Bucket
-from easyaws.sts import STS
 
 def get_metadata_creds():
     creds = Ec2Tool.get_credentials()
@@ -12,29 +11,21 @@ def get_metadata_role_arn():
     print('role arn: ', role_arn)
     return role_arn
 
-def use_s3_with_credentials():
+def list_s3_buckets():
     my_s3 = S3Bucket(bucket_name='my-first-bucket-84629694625')
     ans = my_s3.s3_client.list_buckets()
+    print(ans)
     return ans
 
-def get_sts_credentials(aws_access_key_id, aws_secret_access_key, token, region, role_arn):
-    my_sts = STS(aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key,
-                 token=token, region=region)
-    creds = my_sts.assume_role(RoleArn=role_arn, RoleSessionName='abcde')
-    return creds
 
 def do_all():
     region = 'us-east-1'
     aws_access_key_id, aws_secret_access_key, token = get_metadata_creds()
     role_arn = get_metadata_role_arn()
     try:
-        use_s3_with_credentials()
-
+        list_s3_buckets()
     except Exception as e:
         print('Creds not good!!!', e)
-    print('=*' * 30)
-    # creds = get_sts_credentials(aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key,
-    #                             token=token, region=region, role_arn=role_arn)
-    # print('STS creds: ', creds)
+
 
 do_all()
